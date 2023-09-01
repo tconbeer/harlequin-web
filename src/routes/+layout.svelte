007da55d071e8af6651d185194d599e0629e163f
Ted Conbeer <tconbeer@users.noreply.github.com>
2023-09-01 15:29:43 -0600

    fix: transition and other ui tweaks, close #5

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div
-  class="mx-auto grid max-w-screen-lg grid-rows-[auto_1fr_auto] px-6 selection:bg-pink lg:px-0"
+  class="mx-auto grid max-w-screen-lg grid-rows-[auto_1fr_auto] px-6 selection:bg-pink"
 >
   <Header />
 
