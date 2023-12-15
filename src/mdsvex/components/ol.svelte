2c09ad3c96b65b25f87daefa0360f9565d8e1d07
Ted Conbeer <tconbeer@users.noreply.github.com>
2023-12-14 19:16:56 -0700

    fix: actually, do indent lists

--- src/mdsvex/components/ol.svelte
+++ src/mdsvex/components/ol.svelte
@@ -1,5 +1,5 @@
 <ol
-  class="mb-2 list-inside list-decimal space-y-2 marker:font-bold marker:text-purple"
+  class="mb-2 ml-5 list-outside list-decimal space-y-2 marker:font-bold marker:text-purple"
 >
   <slot />
 </ol>
