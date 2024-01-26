4f7a01b968b43d13e9fd71bb2cca94b69818ed00
Ted Conbeer <tconbeer@users.noreply.github.com>
2024-01-26 15:48:54 -0700

    feat: add history viewer

--- src/routes/features.svelte
+++ src/routes/features.svelte
@@ -5,6 +5,7 @@
   import results from "$lib/assets/features/results.png";
   import full_screen from "$lib/assets/features/full_screen.webp";
   import exporter from "$lib/assets/features/export.png";
+  import history from "$lib/assets/features/history.webp";
 </script>
 
 <p class="text-center">
@@ -45,6 +46,13 @@
       title="Results Export"
       body="Export query results and configure the export using a helpful UI."
       src={exporter}
+    />
+  </li>
+  <li>
+    <Feature
+      title="Query History"
+      body="View a list of executed queries and a summary of results. Select to insert into a new buffer."
+      src={history}
     />
   </li>
 </ul>
