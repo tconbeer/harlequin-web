6c3276bfac648133455db91e0e7a5d4eeec32d30
Ted Conbeer <tconbeer@users.noreply.github.com>
2024-05-03 20:49:07 +0000

    fix: update sveltekit, svelte, and adapter to fix isr bug

--- src/routes/docs/[topic]/[[page]]/+page.ts
+++ src/routes/docs/[topic]/[[page]]/+page.ts
@@ -13,6 +13,6 @@ export async function load({ params }) {
       meta: page.metadata,
     };
   } catch (e) {
-    throw error(404, `Could not find ${params.topic}/${params.page}`);
+    error(404, `Could not find ${params.topic}/${params.page}`);
   }
 }
