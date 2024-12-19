dec97606bcd802f9e0573c9a9178e5f32a244262
Ted Conbeer <tconbeer@users.noreply.github.com>
2024-12-19 16:45:32 -0700

    fiX: update theme info

--- src/docs/themes.md
+++ src/docs/themes.md
@@ -3,12 +3,14 @@ title: Choosing a Theme
 menuOrder: 5
 ---
 
-You can set a theme for Harlequin, passing the name of any [Pygments style](https://pygments.org/styles/) to the `--theme` or `-t` option.
-
-Depending on the number of colors supported by your terminal and shell, some themes [may not look great](troubleshooting/appearance#colors). For any terminal, we can recommend `monokai` (the default), `zenburn`, `github-dark`, `fruity`, `native` and `one-dark`.
+You can set a theme for Harlequin, passing the name of any Textual Theme to the `--theme` or `-t` option.
 
 ```bash
-harlequin --theme one-dark
+harlequin --theme gruvbox
 ```
+
+Depending on the number of colors supported by your terminal and shell, some themes [may not look great](troubleshooting/appearance#colors). For any terminal, we can recommend `harlequin` (the default).
+
+To see a list of theme names, run `harlequin --help`.
 
 For a gallery of many of the better-looking themes, see the [homepage](/#themes).
--- src/routes/themes.svelte
+++ src/routes/themes.svelte
@@ -1,64 +1,37 @@
 <script lang="ts">
   import LazyImage from "$lib/components/lazy_image.svelte";
 
-  import arduino from "$lib/assets/themes/arduino.svg";
+  import catppuccin_latte from "$lib/assets/themes/catppuccin-latte.svg";
+  import catppuccin_mocha from "$lib/assets/themes/catppuccin-mocha.svg";
   import dracula from "$lib/assets/themes/dracula.svg";
-  import emacs from "$lib/assets/themes/emacs.svg";
-  import fruity from "$lib/assets/themes/fruity.svg";
-  import github_dark from "$lib/assets/themes/github-dark.svg";
-  import gruvbox_dark from "$lib/assets/themes/gruvbox-dark.svg";
-  import gruvbox_light from "$lib/assets/themes/gruvbox-light.svg";
+  import flexoki from "$lib/assets/themes/flexoki.svg";
+  import gruvbox from "$lib/assets/themes/gruvbox.svg";
   import harlequin from "$lib/assets/themes/harlequin.svg";
-  import igor from "$lib/assets/themes/igor.svg";
-  import inkpot from "$lib/assets/themes/inkpot.svg";
-  import lightbulb from "$lib/assets/themes/lightbulb.svg";
-  import material from "$lib/assets/themes/material.svg";
   import monokai from "$lib/assets/themes/monokai.svg";
-  import native from "$lib/assets/themes/native.svg";
-  import nord_darker from "$lib/assets/themes/nord-darker.svg";
-  import one_dark from "$lib/assets/themes/one-dark.svg";
-  import perldoc from "$lib/assets/themes/perldoc.svg";
-  import rrt from "$lib/assets/themes/rrt.svg";
-  import solarized_dark from "$lib/assets/themes/solarized-dark.svg";
+  import nord from "$lib/assets/themes/nord.svg";
   import solarized_light from "$lib/assets/themes/solarized-light.svg";
-  import stata_dark from "$lib/assets/themes/stata-dark.svg";
-  import vs from "$lib/assets/themes/vs.svg";
-  import zenburn from "$lib/assets/themes/zenburn.svg";
+  import textual_dark from "$lib/assets/themes/textual-dark.svg";
+  import textual_light from "$lib/assets/themes/textual-light.svg";
+  import tokyo_night from "$lib/assets/themes/tokyo-night.svg";
 
   const themes = [
-    ["arduino", arduino],
+    ["catppuccin-latte", catppuccin_latte],
+    ["catppuccin-mocha", catppuccin_mocha],
     ["dracula", dracula],
-    ["emacs", emacs],
-    ["fruity", fruity],
-    ["github_dark", github_dark],
-    ["gruvbox_dark", gruvbox_dark],
-    ["gruvbox_light", gruvbox_light],
+    ["flexoki", flexoki],
+    ["gruvbox", gruvbox],
     ["harlequin", harlequin],
-    ["igor", igor],
-    ["inkpot", inkpot],
-    ["lightbulb", lightbulb],
-    ["material", material],
     ["monokai", monokai],
-    ["native", native],
-    ["nord_darker", nord_darker],
-    ["one_dark", one_dark],
-    ["perldoc", perldoc],
-    ["rrt", rrt],
-    ["solarized_dark", solarized_dark],
-    ["solarized_light", solarized_light],
-    ["stata_dark", stata_dark],
-    ["vs", vs],
-    ["zenburn", zenburn],
+    ["nord", nord],
+    ["solarized-light", solarized_light],
+    ["textual-dark", textual_dark],
+    ["textual-light", textual_light],
+    ["tokyo-night", tokyo_night],
   ];
 </script>
 
 <p class="text-center">
-  Choose from dozens of <a
-    href="https://pygments.org/styles/"
-    target="_blank"
-    class="underline hover:decoration-green hover:decoration-4 hover:underline-offset-4"
-    >styles</a
-  >.
+  Choose from many of themes, or (coming soon!) make your own.
 </p>
 <ul class="mt-4 flex flex-wrap justify-center gap-4 overflow-visible">
   {#each themes as [name, src]}
