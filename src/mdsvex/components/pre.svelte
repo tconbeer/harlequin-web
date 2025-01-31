fa57fffa94cb9c35d5dbb4a51c8ae673d3d28066
Ted Conbeer <tconbeer@users.noreply.github.com>
2025-01-31 14:02:43 -0700

    feat: add clipboard buttons to pre elements (#112)

--- src/mdsvex/components/pre.svelte
+++ src/mdsvex/components/pre.svelte
@@ -1,11 +1,47 @@
 <script lang="ts">
+  import type { Snippet } from "svelte";
+
   let { children } = $props();
+  import clippy from "$lib/assets/icons/icons8-clipboard-48.png";
+  import check from "$lib/assets/icons/icons8-check-50.png";
+
+  let copy_icon = $state(clippy);
+  const element_id = crypto.randomUUID();
+
+  function copy() {
+    copy_icon = check;
+    setTimeout(() => {
+      copy_icon = clippy;
+    }, 1000);
+    var code_snippet = document.getElementById(element_id)?.innerText.trim();
+    if (code_snippet && code_snippet?.startsWith("$")) {
+      code_snippet = code_snippet.slice(1).trim();
+    }
+    if (code_snippet) {
+      navigator.clipboard.writeText(code_snippet);
+    }
+  }
 </script>
 
-<pre
-  class="my-2 w-full overflow-x-auto rounded border border-purple bg-pink px-4 py-2 align-middle text-sm selection:bg-purple"><code
-    class="inline-block overflow-x-auto align-middle">{@render children()}</code
-  ></pre>
+<div
+  class="flex w-full overflow-x-auto rounded border border-purple bg-pink align-middle"
+>
+  <pre
+    class="my-2 flex-1 overflow-x-auto px-4 py-2 align-middle text-sm selection:bg-purple"><code
+      class="inline-block overflow-x-auto align-middle"
+      id={element_id}>{@render children()}</code
+    ></pre>
+  <button
+    onclick={copy}
+    class="active:rotate-360 group rounded px-2 transition duration-200 hover:bg-green"
+  >
+    <img
+      class="duration-400 h-4 w-4 transition group-hover:scale-110 group-active:translate-x-1 group-active:translate-y-1"
+      src={copy_icon}
+      alt="clipboard icon"
+    />
+  </button>
+</div>
 
 <style>
   /* Firefox */
